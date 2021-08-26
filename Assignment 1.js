357ec230279154b35adcaee52982035b640ef836
Mohit Patni <mohitpatni0731@gmail.com>
2021-08-26 14:05:29 +0530

    Create Assignment 1.js

---
+++ Assignment 1.js
@@ -0,0 +1,40 @@
+// Q1-Declare a variable firstname and initialize it with the value 'Lata'.
+
+var firstname;
+firstname = "Lata";
+
+
+
+// Q2-Which value does x have after execution of the following code?
+  	let x = 'Geeta'; //
+    
+Geeta
+
+
+
+// Q3-Declare a variable flower and assign it the value 'rose'. Declare a second variable tree and assign it the value 'maple'.
+
+var flower;
+flower = "rose";
+var tree;
+tree = "maple";
+
+
+
+// Q4-Which value does x have after execution of the following code?
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;
+
+Hardy
+
+
+
+// Q5-Which value does x have after execution of the following code?
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+
+Toe
